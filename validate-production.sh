779a200bbd153bb37d52e88ade94175df9cad6ad
DilanOjeda <dilanangel12@gmail.com>
2022-03-15 18:11:29 -0400

    feat: add validation containers to production environment

--- validate-production.sh
+++ validate-production.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
-#variables
+# --> change the name
+# --> change the image --name when it is run
+# --> change the port according to Azure Cloud 
+# --> network: database_infranet
+
 #1= container name
-#2= current build number
+#2= build number current
 
 containerName=$1
 previousBuildNumber=$(($2-1))
@@ -19,32 +23,32 @@ if [ "$(docker ps -aq -f name=$containerName)" ] ; then
     fi
 
     docker rmi -f $(docker images -a | grep "converter")
-    
+
     docker run --restart=always -d --name converter \
     -p 9094:9094 \
     -e PORT=9094 \
     -e MONGO_URI=mongodb://mongo/ \
     -e MONGO_DB=upload \
     -e URL=/api/v1/ \
-    -e URLBASE=http://20.25.119.241: \
+    -e URLBASE=http://20.25.80.241: \
     -e FORMATS_SUPPORTED_VIDEO="['mov', 'm4a', '3gp', '3g2', 'mj2', 'mp4']" \
     -e FORMATS_SUPPORTED_IMAGE="['jpeg', 'png', 'jpg']" \
     -e FORMATS_SUPPORTED_DOC="['docx']" \
-    --network mongodbdev_infranet \
+    --network database_infranet \
     dilanof/converter-service:$2
-    
+
 else
-echo "--> create the first converter container"
+    echo "--> create the first converter container"
     docker run --restart=always -d --name converter \
     -p 9094:9094 \
     -e PORT=9094 \
     -e MONGO_URI=mongodb://mongo/ \
     -e MONGO_DB=upload \
     -e URL=/api/v1/ \
-    -e URLBASE=http://20.25.119.241: \
+    -e URLBASE=http://20.25.80.241: \
     -e FORMATS_SUPPORTED_VIDEO="['mov', 'm4a', '3gp', '3g2', 'mj2', 'mp4']" \
     -e FORMATS_SUPPORTED_IMAGE="['jpeg', 'png', 'jpg']" \
     -e FORMATS_SUPPORTED_DOC="['docx']" \
-    --network mongodbdev_infranet \
+    --network database_infranet \
     dilanof/converter-service:$2
 fi
